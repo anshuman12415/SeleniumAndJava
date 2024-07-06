16d370b3c76ccc98bc26145d229960c0803d860b
anshuman yadav <anshumanyadav@anshumans-MacBook-Air.local>
2024-07-06 21:21:25 +0530

    max number finding

--- src/javaProgram/MaxNum.java
+++ src/javaProgram/MaxNum.java
@@ -4,10 +4,18 @@ import java.util.Arrays;
 import java.util.Collections;
 
 public class MaxNum {
-		public static void main(String[] args){
-		Integer[] a = {9,3,6,44,8,5};
-		int max = Collections.max(Arrays.asList(a));
-		System.out.println(max);
-		}
-		}
+	public static void main(String[] args) {
+		Integer[] a = { 9, 3, 6, 44, 8, 5 };
+		// int max = Collections.max(Arrays.asList(a));
+		// System.out.println(max);
 
+		int max = a[0];
+		for (int i = 1; i < a.length; i++) {
+			if (a[i] > max) {
+				max = a[i];
+
+			}
+		}
+		System.out.println(max);
+	}
+}
